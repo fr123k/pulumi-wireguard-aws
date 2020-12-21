c5582207a3e7de14a39d95860f383cce79828353
Frank Ittermann <frank.ittermann@yahoo.de>
2020-12-21 01:04:05 +0100

    Add wireguard deployment to hetzner cloud

---
+++ pkg/hetzner/compute/wireguard.go
@@ -0,0 +1,232 @@
+package compute
+
+import (
+	"github.com/fr123k/pulumi-wireguard-aws/pkg/model"
+	"github.com/fr123k/pulumi-wireguard-aws/pkg/utility"
+
+	"github.com/pulumi/pulumi-hcloud/sdk/go/hcloud"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+)
+
+const size = "t2.large"
+
+//CreateWireguardVM creates a wireguard ec2 aws instance
+func CreateWireguardVM(ctx *pulumi.Context, vpc *model.VpcResult) error {
+
+
+	/*
+		// Enable or disable backups.
+	Backups pulumi.BoolPtrInput
+	// The datacenter name to create the server in.
+	Datacenter pulumi.StringPtrInput
+	// Name or ID of the image the server is created from.
+	Image pulumi.StringInput
+	// ID or Name of an ISO image to mount.
+	Iso pulumi.StringPtrInput
+	// If true, do not upgrade the disk. This allows downgrading the server type later.
+	KeepDisk pulumi.BoolPtrInput
+	// User-defined labels (key-value pairs) should be created with.
+	Labels pulumi.MapInput
+	// The location name to create the server in. `nbg1`, `fsn1` or `hel1`
+	Location pulumi.StringPtrInput
+	// Name of the server to create (must be unique per project and a valid hostname as per RFC 1123).
+	Name pulumi.StringPtrInput
+	// Enable and boot in to the specified rescue system. This enables simple installation of custom operating systems. `linux64` `linux32` or `freebsd64`
+	Rescue pulumi.StringPtrInput
+	// Name of the server type this server should be created with.
+	ServerType pulumi.StringInput
+	// SSH key IDs or names which should be injected into the server at creation time
+	SshKeys pulumi.StringArrayInput
+	// Cloud-Init user data to use during server creation
+	UserData pulumi.StringPtrInput
+	*/
+
+	yaml, err := utility.GetUserData("cloud-init/user-data.txt")
+
+	if err != nil {
+		return err
+	}
+
+	ctx.Export("cloud-init", pulumi.String(*yaml))
+
+	publicKey, err := utility.ReadFile("keys/wireguard.pem.pub")
+	
+	if err != nil {
+		return err
+	}
+
+	sshKey, err := hcloud.NewSshKey(ctx, "wireguard", &hcloud.SshKeyArgs{
+		Name: pulumi.String("wireguard"),
+		PublicKey: pulumi.String(*publicKey),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	server, err := hcloud.NewServer(ctx, "wireguard", &hcloud.ServerArgs{
+		Image: pulumi.String("ubuntu-20.04"),
+		Location: pulumi.String("nbg1"),
+		Name: pulumi.String("wireguard"),
+		ServerType: pulumi.String("cx11"),
+		SshKeys: pulumi.StringArray{
+			sshKey.ID(),
+		},
+		UserData: pulumi.String(*yaml),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	_, err = hcloud.NewServerNetwork(ctx, "srvnetwork", &hcloud.ServerNetworkArgs{
+		ServerId:  utility.IDtoInt(server.CustomResourceState),
+		NetworkId: vpc.IDtoInt(),
+		Ip: pulumi.String("10.8.0.145"),
+	})
+	if err != nil {
+		return err
+	}
+
+	ctx.Export("publicIp", server.Ipv4Address)
+	ctx.Export("publicDns", server.Ipv4Address)
+
+	return nil	
+
+	// sgExternal, err := ec2.NewSecurityGroup(ctx, "wireguard-external", &ec2.SecurityGroupArgs{
+	// 	Description: pulumi.String("Terraform Managed. Allow Wireguard client traffic from internet."),
+	// 	Ingress: ec2.SecurityGroupIngressArray{
+	// 		ec2.SecurityGroupIngressArgs{
+	// 			Protocol:   pulumi.String("udp"),
+	// 			FromPort:   pulumi.Int(51820),
+	// 			ToPort:     pulumi.Int(51820),
+	// 			CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+	// 		},
+	// 		ec2.SecurityGroupIngressArgs{
+	// 			Protocol:   pulumi.String("tcp"),
+	// 			FromPort:   pulumi.Int(22),
+	// 			ToPort:     pulumi.Int(22),
+	// 			CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+	// 		},
+	// 	},
+	// 	Egress: ec2.SecurityGroupEgressArray{
+	// 		ec2.SecurityGroupEgressArgs{
+	// 			Protocol:   pulumi.String("-1"),
+	// 			FromPort:   pulumi.Int(0),
+	// 			ToPort:     pulumi.Int(0),
+	// 			CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+	// 		},
+	// 	},
+	// 	Tags: pulumi.StringMap{
+	// 		"JobUrl":         pulumi.String(os.Getenv("TRAVIS_JOB_WEB_URL")),
+	// 		"Project":        pulumi.String("wireguard"),
+	// 		"pulumi-managed": pulumi.String("True"),
+	// 	},
+	// 	VpcId: vpc.ID(),
+	// })
+	// if err != nil {
+	// 	return err
+	// }
+
+	// sgAdmin, err := ec2.NewSecurityGroup(ctx, "wireguard-admin", &ec2.SecurityGroupArgs{
+	// 	Description: pulumi.String("Terraform Managed. Allow admin traffic internal resources from VPN"),
+	// 	Ingress: ec2.SecurityGroupIngressArray{
+	// 		ec2.SecurityGroupIngressArgs{
+	// 			Protocol:       pulumi.String("-1"),
+	// 			FromPort:       pulumi.Int(0),
+	// 			ToPort:         pulumi.Int(0),
+	// 			SecurityGroups: pulumi.StringArray{sgExternal.ID()},
+	// 		},
+	// 		ec2.SecurityGroupIngressArgs{
+	// 			Protocol:       pulumi.String("icmp"),
+	// 			FromPort:       pulumi.Int(8),
+	// 			ToPort:         pulumi.Int(0),
+	// 			SecurityGroups: pulumi.StringArray{sgExternal.ID()},
+	// 		},
+	// 	},
+	// 	Egress: ec2.SecurityGroupEgressArray{
+	// 		ec2.SecurityGroupEgressArgs{
+	// 			Protocol:   pulumi.String("-1"),
+	// 			FromPort:   pulumi.Int(0),
+	// 			ToPort:     pulumi.Int(0),
+	// 			CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+	// 		},
+	// 	},
+	// 	Tags: pulumi.StringMap{
+	// 		"JobUrl":         pulumi.String(os.Getenv("TRAVIS_JOB_WEB_URL")),
+	// 		"Project":        pulumi.String("wireguard"),
+	// 		"pulumi-managed": pulumi.String("True"),
+	// 	},
+	// 	VpcId: vpc.ID(),
+	// })
+	// if err != nil {
+	// 	return err
+	// }
+
+	// mostRecent := true
+	// //TODO check if jenkins master jocker ami exists use it otherwise use this one.
+	// //make this behaviour configurable always use the following ami except following cases
+	// // 1) jenkins jocker ami exists 2) 1) && env var JENKINS_AMI=ami
+	// ami, err := aws.GetAmi(ctx, &aws.GetAmiArgs{
+	// 	Filters: []aws.GetAmiFilter{
+	// 		{
+	// 			Name:   "name",
+	// 			Values: []string{"ubuntu/images/hvm-ssd/ubuntu-*-18.04-amd64-server-*"},
+	// 		},
+	// 	},
+	// 	Owners:     []string{"099720109477"},
+	// 	MostRecent: &mostRecent,
+	// })
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// //TODO cloud-init use only if jenkins ami doesn't exists.
+	// // yaml, err := getCloudInitYaml("cloud-init/cloud-init.yaml", awsKeyID, awsKeySecret)
+	// yaml, err := utility.GetUserData("cloud-init/user-data.txt")
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// ctx.Export("cloud-init", pulumi.String(*yaml))
+
+	// publicKey, err := utility.ReadFile("keys/wireguard.pem.pub")
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// keyPair, err := ec2.NewKeyPair(ctx, "wireguard", &ec2.KeyPairArgs{
+	// 	KeyName:   pulumi.String("wireguard"),
+	// 	PublicKey: pulumi.String(*publicKey),
+	// })
+
+	// if err != nil {
+	// 	return err
+	// }
+
+	// server, err := ec2.NewInstance(ctx, "wireguard", &ec2.InstanceArgs{
+	// 	AssociatePublicIpAddress: pulumi.Bool(true),
+	// 	Tags: pulumi.StringMap{
+	// 		"Name":   pulumi.String("wireguard"),
+	// 		"JobUrl": pulumi.String(os.Getenv("TRAVIS_JOB_WEB_URL")),
+	// 	},
+	// 	InstanceType: pulumi.String(size),
+	// 	KeyName:      keyPair.KeyName, //create the keypair with pulumi
+	// 	Ami:          pulumi.String(ami.Id),
+	// 	UserData:     pulumi.String(*yaml),
+	// 	SubnetId:     vpc.SubnetResults[0].ID(),
+
+	// 	VpcSecurityGroupIds: pulumi.StringArray{
+	// 		sgExternal.ID(), sgAdmin.ID(),
+	// 	},
+	// })
+
+	// ctx.Export("publicIp", server.PublicIp)
+	// ctx.Export("publicDns", server.PublicDns)
+
+	// return err
+}
---
+++ pkg/hetzner/network/vpc.go
@@ -0,0 +1,103 @@
+package network
+
+import (
+	"strconv"
+
+	"github.com/fr123k/pulumi-wireguard-aws/pkg/model"
+
+	"github.com/pulumi/pulumi-hcloud/sdk/go/hcloud"
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+
+	"github.com/creasty/defaults"
+)
+
+//CreateVPC creates a aws VPC resource
+func CreateVPC(ctx *pulumi.Context, vpcArgs *model.VpcArgs) (*model.VpcResult, error) {
+	defaults.MustSet(vpcArgs)
+
+	network, err := hcloud.NewNetwork(ctx, vpcArgs.Name, &hcloud.NetworkArgs{
+		IpRange: pulumi.String(vpcArgs.Cidr),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	subnets := make([]model.SubnetResult, len(vpcArgs.Subnets))
+	for i, subnetArg := range vpcArgs.Subnets {
+		subnet, err := hcloud.NewNetworkSubnet(ctx, vpcArgs.Name, &hcloud.NetworkSubnetArgs{
+			NetworkId:   network.ID().ApplyInt(func(id pulumi.ID) int {
+				number, err := strconv.Atoi(string(id))
+				if err != nil {
+					panic(err)
+				}
+				return number
+			}),
+			Type:        pulumi.String("cloud"),
+			NetworkZone: pulumi.String("eu-central"),
+			IpRange:     pulumi.String(subnetArg.Cidr),
+		})
+		if err != nil {
+			return nil, err
+		}
+		ctx.Export("Subnet-ID", subnet.ID())
+		subnets[i] = model.SubnetResult{
+			Subnet: subnet.CustomResourceState,
+		}
+	}
+
+	return &model.VpcResult{
+		Vpc: network.CustomResourceState,
+		SubnetResults: subnets,
+	}, nil
+
+	// vpc, err := ec2.NewVpc(ctx, vpcArgs.Name, &ec2.VpcArgs{
+	// 	CidrBlock:          pulumi.String(vpcArgs.Cidr),
+	// 	EnableDnsHostnames: pulumi.Bool(true),
+	// 	EnableDnsSupport:   pulumi.Bool(true),
+	// 	InstanceTenancy:    pulumi.String(vpcArgs.InstanceTenancy),
+	// })
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// // Export IDs of the created resources to the Pulumi stack
+	// ctx.Export("VPC-ID", vpc.ID())
+
+	// internetGW, err := ec2.NewInternetGateway(ctx, vpcArgs.Name, &ec2.InternetGatewayArgs{
+	// 	VpcId: vpc.ID(),
+	// })
+
+	// ec2.NewRoute(ctx, vpcArgs.Name, &ec2.RouteArgs{
+	// 	RouteTableId: vpc.MainRouteTableId,
+	// 	DestinationCidrBlock:   pulumi.String("0.0.0.0/0"),
+	// 	GatewayId: internetGW.ID(),
+	// })
+
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// subnets := make([]model.SubnetResult, len(vpcArgs.Subnets))
+	// for i, subnetArg := range vpcArgs.Subnets {
+	// 	subnet, err := ec2.NewSubnet(ctx, vpcArgs.Name, &ec2.SubnetArgs{
+	// 		VpcId:     vpc.ID(),
+	// 		CidrBlock: pulumi.String(subnetArg.Cidr),
+	// 	})
+	
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
+	
+	// 	ctx.Export("Subnet-ID", subnet.ID())
+	// 	subnets[i] = model.SubnetResult{
+	// 		Subnet: subnet.CustomResourceState,
+	// 	}
+	// }
+
+	// vpcResult := &model.VpcResult{
+	// 	Vpc: vpc.CustomResourceState,
+	// 	SubnetResults: subnets,
+	// }
+	
+	// return vpcResult, nil
+}
