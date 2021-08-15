be175bd52a4381e2e86860ffa7cac6d1e07b2abf
Frank Ittermann <frank.ittermann@yahoo.de>
2021-08-15 15:16:38 +0200

    Add generic ssh command provisioning

--- cmd/wireguard/packer/wireguard.go
+++ cmd/wireguard/packer/wireguard.go
@@ -34,6 +34,16 @@ func main() {
                 Username:   "ubuntu",
                 Timeout:    2 * time.Minute,
                 SSHKeyPair: *keyPair.SSHKeyPair,
+                Commands: []actors.SSHCommand{
+                    {
+                        Command: "sudo cloud-init status --wait",
+                        Output:  false,
+                    },
+                    {
+                        Command: "sudo cat /tmp/server_publickey",
+                        Output:  true,
+                    },
+                },
             },
             utility.Logger{
                 Ctx: ctx,
