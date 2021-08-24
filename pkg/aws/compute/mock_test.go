9b9637d80995ffe503df9ccb31590d3a597fe41c
Frank Ittermann <frank.ittermann@yahoo.de>
2021-08-24 10:35:28 +0200

    Fix readFile of the ProjectFileReader
    Dependening on the current folder the readFile method
    ends up in an infinite loop.
    
    Also add 60 seconds timeout to go unit test execution.

--- pkg/aws/compute/mock_test.go
+++ pkg/aws/compute/mock_test.go
@@ -1,21 +1,21 @@
 package compute
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
-	"testing"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "path/filepath"
+    "testing"
 
-	"github.com/fr123k/pulumi-wireguard-aws/pkg/model"
-	"github.com/fr123k/pulumi-wireguard-aws/pkg/utility"
-	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/stretchr/testify/assert"
+    "github.com/fr123k/pulumi-wireguard-aws/pkg/model"
+    "github.com/fr123k/pulumi-wireguard-aws/pkg/utility"
+    "github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+    "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+    "github.com/stretchr/testify/assert"
 )
 
 type mocks int
+
 //TODO reduce code lines
 //TODO reduce complexity for testing
 func (mocks) NewResource(args pulumi.MockResourceArgs) (string, resource.PropertyMap, error) {
@@ -33,8 +33,8 @@ func (mocks) Call(args pulumi.MockCallArgs) (resource.PropertyMap, error) {
     fmt.Printf("Mock Called %s\n", args.Token)
     //TODO implement mock based on invocation count and args
     if args.Token == "aws:ec2/getAmiIds:getAmiIds" {
-            outputs["architecture"] = "x86_64"
-            outputs["ids"] = []string{"ami-0eb1f3cdeeb8eed2a"}
+        outputs["architecture"] = "x86_64"
+        outputs["ids"] = []string{"ami-0eb1f3cdeeb8eed2a"}
     }
     return resource.NewPropertyMapFromMap(outputs), nil
 }
@@ -45,10 +45,23 @@ type ProjectRootFileReader struct {
 // ReadFile read the file content from a string in the memory instead of the filesystem
 func (fileReader ProjectRootFileReader) ReadFile(filename string) ([]byte, error) {
     wd, _ := os.Getwd()
-    for !strings.HasSuffix(wd, "pulumi-wireguard-aws") {
+    for !Exists(fmt.Sprintf("%s/%s", wd, filename)) {
         wd = filepath.Dir(wd)
+        if len(wd) <= 1 {
+            fmt.Println(wd)
+            break
+        }
     }
     return ioutil.ReadFile(fmt.Sprintf("%s/%s", wd, filename))
+}
+
+func Exists(name string) bool {
+    if _, err := os.Stat(name); err != nil {
+        if os.IsNotExist(err) {
+            return false
+        }
+    }
+    return true
 }
 
 func ProjectFileContent() {
