513857895b52ea3accc8864692957b460605292d
Frank Ittermann <frankittermann@Franks-MacBook-Pro.local>
2024-11-02 00:49:42 +0100

    chore: update all go dependencies

--- cmd/jenkins/aws/jenkins.go
+++ cmd/jenkins/aws/jenkins.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    "github.com/fr123k/pulumi-wireguard-aws/pkg/aws/compute"
-    "github.com/fr123k/pulumi-wireguard-aws/pkg/aws/network"
-    "github.com/fr123k/pulumi-wireguard-aws/pkg/model"
-    "github.com/fr123k/pulumi-wireguard-aws/pkg/shared"
-    "github.com/pulumi/pulumi-aws/sdk/v4/go/aws/iam"
-    "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-    "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+	"github.com/fr123k/pulumi-wireguard-aws/pkg/aws/compute"
+	"github.com/fr123k/pulumi-wireguard-aws/pkg/aws/network"
+	"github.com/fr123k/pulumi-wireguard-aws/pkg/model"
+	"github.com/fr123k/pulumi-wireguard-aws/pkg/shared"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/iam"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func exports(ctx *pulumi.Context, infra *compute.Infrastructure) {
-    ctx.Export("publicIp", infra.Server.PublicIp)
-    ctx.Export("publicDns", infra.Server.PublicDns)
+	ctx.Export("publicIp", infra.Server.PublicIp)
+	ctx.Export("publicDns", infra.Server.PublicDns)
 }
 
 func main() {
-    pulumi.Run(func(ctx *pulumi.Context) error {
-        const username = "pulumi-automation"
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		const username = "pulumi-automation"
 
-        iamUser, err := iam.NewUser(ctx, username+"-user", &iam.UserArgs{
-            Tags: pulumi.StringMap{
-                "Creator": pulumi.String("jenkins-aws-pulumi"),
-                "JobUrl":  pulumi.String(os.Getenv("TRAVIS_JOB_WEB_URL")),
-            },
-        })
+		iamUser, err := iam.NewUser(ctx, username+"-user", &iam.UserArgs{
+			Tags: pulumi.StringMap{
+				"Creator": pulumi.String("jenkins-aws-pulumi"),
+				"JobUrl":  pulumi.String(os.Getenv("TRAVIS_JOB_WEB_URL")),
+			},
+		})
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        var s3PolicyContent = `{
+		var s3PolicyContent = `{
 		"Version": "2012-10-17",
 		"Statement": [
 			{
@@ -43,7 +43,7 @@ func main() {
 		]
 	}`
 
-        var ec2PolicyContent = `{
+		var ec2PolicyContent = `{
 		"Version": "2012-10-17",
 		"Statement": [
 			{
@@ -55,7 +55,7 @@ func main() {
 	}
 	`
 
-        var iamPolicyContent = `{
+		var iamPolicyContent = `{
 		"Version": "2012-10-17",
 		"Statement": [
 			{
@@ -67,113 +67,113 @@ func main() {
 	}
 	`
 
-        s3IamPolicy, err := iam.NewPolicy(ctx, username+"-user-policy-s3", &iam.PolicyArgs{
-            Policy: pulumi.String(s3PolicyContent),
-        })
+		s3IamPolicy, err := iam.NewPolicy(ctx, username+"-user-policy-s3", &iam.PolicyArgs{
+			Policy: pulumi.String(s3PolicyContent),
+		})
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        ec2IamPolicy, err := iam.NewPolicy(ctx, username+"-user-policy-ec2", &iam.PolicyArgs{
-            Policy: pulumi.String(ec2PolicyContent),
-        })
+		ec2IamPolicy, err := iam.NewPolicy(ctx, username+"-user-policy-ec2", &iam.PolicyArgs{
+			Policy: pulumi.String(ec2PolicyContent),
+		})
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        iamIamPolicy, err := iam.NewPolicy(ctx, username+"-user-policy-iam", &iam.PolicyArgs{
-            Policy: pulumi.String(iamPolicyContent),
-        })
+		iamIamPolicy, err := iam.NewPolicy(ctx, username+"-user-policy-iam", &iam.PolicyArgs{
+			Policy: pulumi.String(iamPolicyContent),
+		})
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        iam.NewUserPolicyAttachment(ctx, username+"-user-policy-attachment-s3", &iam.UserPolicyAttachmentArgs{
-            User:      iamUser.ID(),
-            PolicyArn: s3IamPolicy.ID(),
-        })
+		iam.NewUserPolicyAttachment(ctx, username+"-user-policy-attachment-s3", &iam.UserPolicyAttachmentArgs{
+			User:      iamUser.ID(),
+			PolicyArn: s3IamPolicy.ID(),
+		})
 
-        iam.NewUserPolicyAttachment(ctx, username+"-user-policy-attachment-ec2", &iam.UserPolicyAttachmentArgs{
-            User:      iamUser.ID(),
-            PolicyArn: ec2IamPolicy.ID(),
-        })
+		iam.NewUserPolicyAttachment(ctx, username+"-user-policy-attachment-ec2", &iam.UserPolicyAttachmentArgs{
+			User:      iamUser.ID(),
+			PolicyArn: ec2IamPolicy.ID(),
+		})
 
-        iam.NewUserPolicyAttachment(ctx, username+"-user-policy-attachment-iam", &iam.UserPolicyAttachmentArgs{
-            User:      iamUser.ID(),
-            PolicyArn: iamIamPolicy.ID(),
-        })
+		iam.NewUserPolicyAttachment(ctx, username+"-user-policy-attachment-iam", &iam.UserPolicyAttachmentArgs{
+			User:      iamUser.ID(),
+			PolicyArn: iamIamPolicy.ID(),
+		})
 
-        iamKeys, err := iam.NewAccessKey(ctx, username+"-user-keys", &iam.AccessKeyArgs{
-            User: iamUser.ID(),
-        })
+		iamKeys, err := iam.NewAccessKey(ctx, username+"-user-keys", &iam.AccessKeyArgs{
+			User: iamUser.ID(),
+		})
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        ctx.Export("AccessKeys", iamKeys.ID())
-        ctx.Export("AccessKeysSecret", iamKeys.Secret)
-        return createInfraStructure(ctx)
-    })
+		ctx.Export("AccessKeys", iamKeys.ID())
+		ctx.Export("AccessKeysSecret", iamKeys.Secret)
+		return createInfraStructure(ctx)
+	})
 }
 
 func createInfraStructure(ctx *pulumi.Context) error {
-    config := config.New(ctx, "")
+	config := config.New(ctx, "")
 
-    //TODO fetch new created aws key and secret
-    // awsKeyID := config.Require("key")
-    // awsKeySecret := config.Require("secret")
+	//TODO fetch new created aws key and secret
+	// awsKeyID := config.Require("key")
+	// awsKeySecret := config.Require("secret")
 
-    userData, err := shared.JenkinsUserData("cloud-init/jenkins.yaml")
-    if err != nil {
-        return err
-    }
+	userData, err := shared.JenkinsUserData("cloud-init/jenkins.yaml")
+	if err != nil {
+		return err
+	}
 
-    security := model.NewSecurityArgsForVPC(config.GetBool("vpn_enabled_ssh"), model.VPCArgsDefault)
-    security.Println()
+	security := model.NewSecurityArgsForVPC(config.GetBool("vpn_enabled_ssh"), model.VPCArgsDefault)
+	security.Println()
 
-    vpc, err := network.CreateVPC(ctx, model.VPCArgsDefault)
-    if err != nil {
-        return err
-    }
+	vpc, err := network.CreateVPC(ctx, model.VPCArgsDefault)
+	if err != nil {
+		return err
+	}
 
-    keyPairName := "development"
-    keyPair := model.NewKeyPairArgsWithRandomNameAndKey(&keyPairName)
-    computeArgs := model.NewComputeArgsWithKeyPair(vpc, security, keyPair)
-    computeArgs.Name = "jenkins-master"
-    computeArgs.UserData = userData
-    computeArgs.Images = []*model.ImageArgs{
-        {
-            Name:   "ubuntu/images/hvm-ssd/ubuntu-*-18.04-amd64-server-*",
-            Owners: []string{"099720109477"},
-        },
-    }
+	keyPairName := "development"
+	keyPair := model.NewKeyPairArgsWithRandomNameAndKey(&keyPairName)
+	computeArgs := model.NewComputeArgsWithKeyPair(vpc, security, keyPair)
+	computeArgs.Name = "jenkins-master"
+	computeArgs.UserData = userData
+	computeArgs.Images = []*model.ImageArgs{
+		{
+			Name:   "ubuntu/images/hvm-ssd/ubuntu-*-18.04-amd64-server-*",
+			Owners: []string{"099720109477"},
+		},
+	}
 
-    tags := map[string]string{
-        "JobUrl":         os.Getenv("TRAVIS_JOB_WEB_URL"),
-        "Project":        "jenkins",
-        "pulumi-managed": "True",
-    }
+	tags := map[string]string{
+		"JobUrl":         os.Getenv("TRAVIS_JOB_WEB_URL"),
+		"Project":        "jenkins",
+		"pulumi-managed": "True",
+	}
 
-    computeArgs.SecurityGroups = shared.JenkinsSecGroup(tags, security)
+	computeArgs.SecurityGroups = shared.JenkinsSecGroup(tags, security)
 
-    vm, err := compute.CreateServer(ctx, computeArgs, exports)
+	vm, err := compute.CreateServer(ctx, computeArgs, exports)
 
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
-    sshConnector := shared.JenkinsProvisioner(ctx, keyPair)
+	sshConnector := shared.JenkinsProvisioner(ctx, keyPair)
 
-    compute.ProvisionVM(ctx, &model.ProvisionArgs{
-        ExportName: "wireguard.publicKey",
-        SourceCompute: &model.ComputeResult{
-            Compute: vm.Server.CustomResourceState,
-        },
-    }, &sshConnector)
+	compute.ProvisionVM(ctx, &model.ProvisionArgs{
+		ExportName: "wireguard.publicKey",
+		SourceCompute: &model.ComputeResult{
+			Compute: vm.Server.CustomResourceState,
+		},
+	}, &sshConnector)
 
-    return err
+	return err
 }
--- pkg/aws/compute/assertions_test.go
+++ pkg/aws/compute/assertions_test.go
@@ -1,71 +1,71 @@
 package compute
 
 import (
-    "sync"
-    "testing"
+	"sync"
+	"testing"
 
-    "github.com/pulumi/pulumi-aws/sdk/v4/go/aws/ec2"
-    "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-    "github.com/stretchr/testify/assert"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/ec2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/stretchr/testify/assert"
 )
 
 type assertion = func(t assert.TestingT, s interface{}, contains interface{}, msg string, args ...interface{}) bool
 
 func assertTags(t *testing.T, infra *Infrastructure, wg *sync.WaitGroup, expected interface{}, assertFnc assertion) {
-    wg.Add(1)
-    pulumi.All(infra.Server.URN(), infra.Server.Tags).ApplyT(func(all []interface{}) error {
-        urn := all[0].(pulumi.URN)
-        tags := all[1].(map[string]string)
+	wg.Add(1)
+	pulumi.All(infra.Server.URN(), infra.Server.Tags).ApplyT(func(all []interface{}) error {
+		urn := all[0].(pulumi.URN)
+		tags := all[1].(map[string]string)
 
-        assertFnc(t, tags, expected, "missing a Name tag on server %v", urn)
-        wg.Done()
-        return nil
-    })
+		assertFnc(t, tags, expected, "missing a Name tag on server %v", urn)
+		wg.Done()
+		return nil
+	})
 }
 
 func assertUserDataNotNil(t *testing.T, infra *Infrastructure, wg *sync.WaitGroup) {
-    wg.Add(1)
-    pulumi.All(infra.Server.URN(), infra.Server.UserData).ApplyT(func(all []interface{}) error {
-        urn := all[0].(pulumi.URN)
-        userData := all[1].(string)
+	wg.Add(1)
+	pulumi.All(infra.Server.URN(), infra.Server.UserData).ApplyT(func(all []interface{}) error {
+		urn := all[0].(pulumi.URN)
+		userData := all[1].(string)
 
-        assert.NotNil(t, userData, "expect userData set on server on server %v", urn)
-        assert.NotEmpty(t,userData, "expect userData set on server on server %v", urn)
+		assert.NotNil(t, userData, "expect userData set on server on server %v", urn)
+		assert.NotEmpty(t, userData, "expect userData set on server on server %v", urn)
 
-        wg.Done()
-        return nil
-    })
+		wg.Done()
+		return nil
+	})
 }
 
 func assertSSHPort(t *testing.T, infra *Infrastructure, wg *sync.WaitGroup, public bool) {
-    wg.Add(1)
-    pulumi.All(infra.Groups[0].URN(), infra.Groups[0].Ingress, infra.Groups[1].Ingress).ApplyT(func(all []interface{}) error {
-        urn := all[0].(pulumi.URN)
+	wg.Add(1)
+	pulumi.All(infra.Groups[0].URN(), infra.Groups[0].Ingress, infra.Groups[1].Ingress).ApplyT(func(all []interface{}) error {
+		urn := all[0].(pulumi.URN)
 
-        ingress := append(all[1].([]ec2.SecurityGroupIngress), all[2].([]ec2.SecurityGroupIngress)...)
+		ingress := append(all[1].([]ec2.SecurityGroupIngress), all[2].([]ec2.SecurityGroupIngress)...)
 
-        assert.Len(t, ingress, 4, "expect 4 ingress security rules set on server")
+		assert.Len(t, ingress, 4, "expect 4 ingress security rules set on server")
 
-        for _, i := range ingress {
-            openToInternet := false
-            if i.ToPort == 22 {
-                for _, b := range i.CidrBlocks {
-                    if b == "0.0.0.0/0" {
-                        openToInternet = true
-                        break
-                    }
-                }
-            }
+		for _, i := range ingress {
+			openToInternet := false
+			if i.ToPort == 22 {
+				for _, b := range i.CidrBlocks {
+					if b == "0.0.0.0/0" {
+						openToInternet = true
+						break
+					}
+				}
+			}
 
-            if i.FromPort != 22 {
-                continue
-            }
-            assert.Equal(t, "tcp", i.Protocol, "Expect protocol 'tcp' for ssh rule on group %v", urn)
-            assert.Falsef(t, openToInternet && !public, "illegal SSH port 22 open to the Internet (CIDR 0.0.0.0/0) on group %v", urn)
+			if i.FromPort != 22 {
+				continue
+			}
+			assert.Equal(t, "tcp", i.Protocol, "Expect protocol 'tcp' for ssh rule on group %v", urn)
+			assert.Falsef(t, openToInternet && !public, "illegal SSH port 22 open to the Internet (CIDR 0.0.0.0/0) on group %v", urn)
 
-        }
+		}
 
-        wg.Done()
-        return nil
-    })
+		wg.Done()
+		return nil
+	})
 }
